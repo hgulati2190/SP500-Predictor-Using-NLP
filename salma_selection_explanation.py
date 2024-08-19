57d9ffa16995aaa9b02dbdbc19c698991f4d6db4
salma-w <salma.wahwah@yahoo.com>
2024-08-18 19:16:31 -0700

    change path

--- salma_selection_explanation.py
+++ salma_selection_explanation.py
@@ -11,16 +11,16 @@ import datetime
 
 
 # Load the models and transformers
-with open('salma_log_reg.pkl', 'rb') as file:
+with open('salma/salma_log_reg.pkl', 'rb') as file:
     log_reg = pickle.load(file)
 
-with open('salma_xgb_model.pkl', 'rb') as file:
+with open('salma/salma_xgb_model.pkl', 'rb') as file:
     xgb_model = pickle.load(file)
 
-with open('salma_vectorizer.pkl', 'rb') as file:
+with open('salma/salma_vectorizer.pkl', 'rb') as file:
     vectorizer = pickle.load(file)
 
-with open('salma_scaler.pkl', 'rb') as file:
+with open('salma/salma_scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
     
 
@@ -42,7 +42,7 @@ functions_dict = {
     'fetch_spy_data': fetch_spy_data
 }
 
-save_functions(functions_dict, 'salma_functions.pkl')
+save_functions(functions_dict, 'salma/salma_functions.pkl')
 
 
 # In[198]:
